c7eb37d3e54e7170bed17db811e86eb25e491160
naimulislammunna <naimulislammunna742@gmail.com>
2024-08-29 01:02:52 +0600

    added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser')
+// const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 6000;
+const port = process.env.port || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+// app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('client server ok')
@@ -18,3 +18,32 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('port is running:', port);
 })
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@express-explore.use1c.mongodb.net/?retryWrites=true&w=majority&appName=express-explore`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db('pokids').collection('services');
+
+        app.get('/services', async(req, res)=>{
+            const cursor = await collection.find().toArray();
+            res.send(cursor)
+        });
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
